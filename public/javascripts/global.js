26e26fda7d81343b8394b8455cf5b97bb3bb74dc
renanreboredor3 <renanreboredo@paramag.com.br>
2016-11-16 23:07:47 -0200

    Adding the 'User Info' Box on index.jade and configuring ajax to show data from user on global.js

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
 	// Populate the user table on initial page load
 	populateTable();
 
+	// Username Link Click
+	$('#userList table tbody').on('click','td a.linkshowuser', showUserInfo);
+
 });
 
 // Functions =================================
@@ -19,9 +22,11 @@ var populateTable = function (){
 	// jQuery AJAX call for JSON
 	$.getJSON('/users/userlist', function(data){
 
+		userListData = data;
+	
 		// For each item in our JSON, add a table row 
 		// and cells to the content string
-      $.each(data, function(){
+    $.each(data, function(){
           tableContent += '<tr>';
           tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
           tableContent += '<td>' + this.email + '</td>';
@@ -33,3 +38,25 @@ var populateTable = function (){
 		$('#userList table tbody').html(tableContent);
 	});
 }
+
+// Show User info
+var showUserInfo = function(event){
+	// Prevent Link from Firing
+	event.preventDefault();
+
+	// Retrieve username from link rel attribute
+	var thisUserName = $(this).attr('rel');
+
+	// Get index of object based on id value
+	var arrayPosition = userListData.map(function(arrayItem){ return arrayItem.username; }).indexOf(thisUserName);
+
+	// Get User Object
+	var thisUserObject = userListData[arrayPosition];
+
+	// Populate Info Box
+	$('#userInfoName').text(thisUserObject.fullname);
+  $('#userInfoAge').text(thisUserObject.age);
+  $('#userInfoGender').text(thisUserObject.gender);
+  $('#userInfoLocation').text(thisUserObject.location);
+}
+
